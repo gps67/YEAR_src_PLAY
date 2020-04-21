b3a7d116838378573d5c66c5dd83477221b56d8f
gps <gps@E6320.dododone.xyz>
2020-04-21 02:00:02 +0100

    exec

--- SCRATCH_ZONE_20/in_Tcl_Module/src_tcl_c_syslog/test_syslog5.sh
+++ SCRATCH_ZONE_20/in_Tcl_Module/src_tcl_c_syslog/test_syslog5.sh
@@ -2,5 +2,5 @@
 
 	PATH=$PATH:
 
-	prog_syslog5 $0 0 USER INFO "SH message without pid"
-	prog_syslog5 $0 1 USER INFO "SH message with pid - pointless (PPID pref)"
+	prog_syslog5.exec $0 0 USER INFO "SH message without pid"
+	prog_syslog5.exec $0 1 USER INFO "SH message with pid - pointless (PPID pref)"

0ceba4222a30361c0203f0aa5a854001b50fecbe
Graham Swallow <gps@information-cascade.co.uk>
2020-06-16 00:05:50 +0100

    tcl_bench is a Package Require module

---
+++ SCRATCH_ZONE_20/in_Tcl_Module/tcl_Bench/tcl_Bench_env.tcl
@@ -0,0 +1,10 @@
+# this is SITE_env.tcl
+# it would be part of auto_path
+# TODO convert to package require tcl_Bench ;# _LIBR
+
+
+# it should edit auto_path 
+# that adds varius CMD
+# it might load MMAP for SESS_DATA
+
+

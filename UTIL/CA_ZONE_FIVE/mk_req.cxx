688e1df04026b6e02c1b9fac4ff970ec42b2360e
Graham Swallow <gps67@giyhub.com>
2020-08-16 22:48:48 +0100

    catchup

--- UTIL/CA_ZONE_FIVE/mk_req.cxx
+++ UTIL/CA_ZONE_FIVE/mk_req.cxx
@@ -42,6 +42,92 @@ void C_progress_genkey( int a, int b, void * cb_arg )
 using namespace CA1;
 
 
+bool DEMO_mk_VPN_GW( str0 ABBR )
+{
+	buffer1 NAME;
+	NAME.print("GW_%s", ABBR );
+	return FAIL("TODO");
+}
+
+bool DEMO_mk_VPN_PC( str0 ABBR )
+{
+	buffer1 NAME;
+	NAME.print("PC_%s", ABBR );
+	SITE_X509_tag_enum ISS_tag = is_C_pc;
+	obj_hold<MYSITE_X509_layout> mysite_scheme = new MYSITE_X509_layout();
+	obj_hold<MYSITE_CA_task> task = new MYSITE_CA_task(  mysite_scheme, ISS_tag );
+
+	/*
+		VALS <== -code- tag
+
+		that fails - which ZONE is ITEM in 
+	if(!task->MYSITE_SET_DEMO_VALUE_FOR( ISS_tag )) return FAIL_FAILED();
+
+		OBTAIN CA_ZONE	ZONE
+		CREATE  C_ITEM  USER
+
+		OBTAIN CA_ZONE	ZONE_FIVE
+		CREATE CA_LIST	USERS 
+
+		OBTAIN CA_LIST	USER
+		CREATE  C_ITEM  USER_0xFFFF
+
+	*/
+
+	/*
+		VALS => REQ => ISS => files
+	*/
+	if(!task->MYSITE_mk_cert_from_VALS()) return FAIL_FAILED();
+	return PASS("X509");
+}
+
+bool DEMO_mk_cert_group()
+{
+	// mk_LAPTOP
+	if(!DEMO_mk_VPN_PC("DWS_")) return FAIL_FAILED();
+	if(!DEMO_mk_VPN_PC("AMS_10")) return FAIL_FAILED();
+	if(!DEMO_mk_VPN_PC("GPS_E6320")) return FAIL_FAILED();
+	if(!DEMO_mk_VPN_PC("MBS")) return FAIL_FAILED();
+	if(!DEMO_mk_VPN_GW("HUB_9")) return FAIL_FAILED();
+
+	return PASS("OK");
+
+	SITE_X509_tag_enum ISS_tag = is_CA_ZONE;
+
+	/*
+		if(1)	if(!DEMO_mk_cert(is_CA_ONE)) return FAIL_FAILED();
+		if(1)	if(!DEMO_mk_cert(is_CA_ZONE)) return FAIL_FAILED();
+		if(1)	if(!DEMO_mk_cert(is_C_pc)) return FAIL_FAILED();
+		if(1)	if(!DEMO_mk_cert(is_C_user)) return FAIL_FAILED();
+		if(1)	if(!DEMO_mk_cert(is_C_server)) return FAIL_FAILED();
+		if(1)	if(!DEMO_mk_cert(is_C_item)) return FAIL_FAILED();
+		You have two virtual classes, layout and task
+		You also have this parent _DEMO_ caller
+		Change all 3 for your site,
+
+		the current basis, is that you set VALS
+		then bounce it through REQ and ISS
+		then save the result
+	*/
+	obj_hold<MYSITE_X509_layout> mysite_scheme = new MYSITE_X509_layout();
+	obj_hold<MYSITE_CA_task> task = new MYSITE_CA_task(  mysite_scheme, ISS_tag );
+
+	/*
+		VALS <== -code- tag
+
+		that fails - which ZONE is ITEM in 
+	*/
+	if(!task->MYSITE_SET_DEMO_VALUE_FOR( ISS_tag )) return FAIL_FAILED();
+
+	/*
+		VALS => REQ => ISS => files
+	*/
+	if(!task->MYSITE_mk_cert_from_VALS()) return FAIL_FAILED();
+
+	return PASS("DONE");
+}
+		//////////////////////////////////////////////
+
 bool DEMO_mk_cert(
 	SITE_X509_tag_enum ISS_tag
 )
@@ -85,7 +171,7 @@ bool_main(int argc, char *argv[])
 
 	const char * progname = *argv++;
 	argc--;
-	char * argv_default[] = { "ALL", NULL };
+	char * argv_default[] = { "ALL", NULL }; // const
 	if(argc==0) {
 		argv = argv_default;
 		argc= 1;
@@ -127,7 +213,9 @@ bool_main(int argc, char *argv[])
 			if(!DEMO_mk_cert(is_C_item)) return FAIL_FAILED();
 		} else
 		if(arg == "ALL") {
+			if(!DEMO_mk_cert_group()) return FAIL_FAILED();
 
+#if 0
 		if(1)	if(!DEMO_mk_cert(is_CA_ONE)) return FAIL_FAILED();
 		if(1)	if(!DEMO_mk_cert(is_CA_ZONE)) return FAIL_FAILED();
 		if(1)	if(!DEMO_mk_cert(is_C_pc)) return FAIL_FAILED();
@@ -142,6 +230,7 @@ bool_main(int argc, char *argv[])
 		if(1)	if(!DEMO_mk_cert(is_C_item)) return FAIL_FAILED();
 		if(1)	if(!DEMO_mk_cert(is_C_item)) return FAIL_FAILED();
 		if(1)	if(!DEMO_mk_cert(is_C_item)) return FAIL_FAILED();
+#endif
 
 		} else {
 			return FAIL("unrecognised arg '%s'", (STR0) arg );

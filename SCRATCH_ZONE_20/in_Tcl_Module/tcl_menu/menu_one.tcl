bbf5936f2f1dfe60c420674a47da720ead9a4b70
Graham Swallow <gps67@giyhub.com>
2020-08-17 12:58:04 +0100

    pack big fill both

--- SCRATCH_ZONE_20/in_Tcl_Module/tcl_menu/menu_one.tcl
+++ SCRATCH_ZONE_20/in_Tcl_Module/tcl_menu/menu_one.tcl
@@ -3,12 +3,6 @@
 package require Tk
 lappend auto_path [pwd]
 
-proc PATH_add_gvim {} {
-	set DIR1 {C:/Program Files (x86)/Vim/vim82}
-	append ::env(PATH) ";$DIR1"
-	puts $::env(PATH) 
-}
-PATH_add_gvim 
 
 set w1 .w1
 set w1 .
@@ -26,21 +20,30 @@ h_pack $menu_m1
 # text_out is under dot
 mk_text_out_global {} ;# " .text_out %s.%s {} "text_out":w
 
+PATH_add_gvim 
+
 menu_id_text_cmd_Exit $menu_m1
 
-menu_id_text_cmd $menu_m1 [mk_id] "Hello World" {
+if {0} {
+ menu_id_text_cmd $menu_m1 [mk_id] "Hello World" {
     text_out_ln - "Hello, world";
-}
+ }
 
-menu_id_text_cmd $menu_m1 [mk_id] "RUN wish menu_one.tcl" {
+ menu_id_text_cmd $menu_m1 [mk_id] "RUN wish menu_one.tcl" {
     text_out_ln - [ exec wish menu_one.tcl]
-}
+ }
 
-menu_id_text_cmd $menu_m1 [mk_id] "RUN wish wish_list.tcl" {
+ menu_id_text_cmd $menu_m1 [mk_id] "RUN wish wish_list.tcl" {
     text_out_ln - [ exec wish wish_list.tcl]
+ }
 }
 
-menu_ITEM_exec   $menu_m1 { wish wish_list.tcl }
+menu_ITEM_edit   $menu_m1 { wish_list.tcl }
+menu_ITEM_edit   $menu_m1 { menu_one.tcl }
+menu_ITEM_edit   $menu_m1 { MENU/menu_one.tcl }
+menu_ITEM_wish   $menu_m1 { wish_list.tcl }
+menu_ITEM_wish   $menu_m1 { menu_one.tcl }
+menu_ITEM_wish   $menu_m1 { MENU/menu_one.tcl }
 menu_ITEM_script $menu_m1 { puts stdout "hello" }
 menu_ITEM_script $menu_m1 { text_out_ln - "Button Two"; }
 menu_ITEM_exec   $menu_m1 { openvpn }
--- SCRATCH_ZONE_20/in_Tcl_Module/tcl_menu/pack.tcl
+++ SCRATCH_ZONE_20/in_Tcl_Module/tcl_menu/pack.tcl
@@ -2,4 +2,5 @@
 proc v_pack {item} { pack $item -side top -fill x }
 proc v_pack_big {item} { pack $item -side top -fill x -expand y }
 proc h_pack {item} { pack $item -side left -fill y }
-proc h_pack_big {item} { pack $item -side left -fill y -expand y }
+proc h_pack_big {item} { pack $item -side left -fill both -expand y }
+# proc h_pack_big {item} { pack $item -side left -fill y -expand y }

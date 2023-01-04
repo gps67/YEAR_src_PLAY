1840480446ffb32cb87dcf018a26b48566f3dc69
gps <gps67@github.com>
2023-01-04 01:46:29 +0000

    sync

--- UTIL/copy_restart/src/copy_file_dir.cxx
+++ UTIL/copy_restart/src/copy_file_dir.cxx
@@ -18,11 +18,63 @@ struct sender_t {
 
 	u64 size_full = 0;
 
+	// stats
+	u64 size_sent = 0;
+
+	bool re_init()
+	{
+		size_full = 0;
+		size_sent = 0;
+		fd_src.close();
+		fd_dst.close();
+		return true;
+	}
+
 	bool copy_src_name_dst_try(
 		const char * src_over,
 		const char * src_name_ext,
 		const char * dst_over
-	 );
+	);
+
+	bool skip_file_named( str0 name )
+	{
+		if( name.has_prefix(".tmp.")) {
+			// INFO("SKIPPING OWN %s", (STR0) name );
+			return true;
+		}
+		if( name.has_suffix(".cpy")) {
+			// INFO("SKIPPING OWN %s", (STR0) name );
+			return true;
+		}
+		return false; // OK dont skip
+	}
+
+	bool pre_flight_checks()
+	{
+	 	return true;
+	}
+
+	bool call_fsync() {
+		e_print("f_");
+		if(! fd_dst.fsync() ) {
+			return FAIL_FAILED();
+		}
+		e_print("\b");
+		size_sent = 0;
+		return true;
+	}
+
+	bool call_fdatasync() {
+		e_print("D_");
+		if(! fd_dst.fdatasync() ) {
+			return FAIL_FAILED();
+		}
+		e_print("\b");
+		size_sent = 0;
+		return true;
+	}
+
+	
 };
 
 bool sender_t::copy_src_name_dst_try(
@@ -30,14 +82,9 @@ bool sender_t::copy_src_name_dst_try(
 	const char * src_name_ext,
 	const char * dst_over
  ) {
-
-	if(0) // did we get here
- 	 INFO("%s", src_name_ext );
+	re_init();
 
 //	gdb_invoke(false);
-
-//	dir_name_ext test_dir_name_ext;
-//	test_dir_name_ext.test1();
 
 	if(0) {
 	INFO("src_over     %s", src_over );
@@ -45,16 +92,22 @@ bool sender_t::copy_src_name_dst_try(
 	INFO("dst_over     %s", dst_over );
 	}
 
-	str1 name = src_name_ext;
-	if( name.has_prefix(".tmp.")) {
-		INFO("SKIPPING OWN %s", (STR0) name );
-		return true;
+	// dst_over must exist as a dir 
+	{
+	 file_stat dst_over_stat; // temp to this { block }
+	 if( !dst_over_stat.stat_expect_is_dir( dst_over )) {
+		return FAIL_FAILED();
+	 }
 	}
-	if( name.has_suffix(".cpy")) {
-		INFO("SKIPPING OWN %s", (STR0) name );
+
+	// do not copy residual files from own naming system
+	// maybe other files from other systems too
+	if( skip_file_named( src_name_ext )) {
+		INFO("SKIPPING OWN %s", src_name_ext );
 		return true;
 	}
 
+	// recompute src_name
 	buffer1 src_name;
 	if( src_over )
 		src_name.print("%s/%s", src_over, src_name_ext );
@@ -63,14 +116,6 @@ bool sender_t::copy_src_name_dst_try(
 
 	file_stat src_stat;
 	file_stat dst_stat;
-	size_full = 0;
-
-	// dst_name must exist as a dir 
-	file_stat dst_over_stat;
-	if( !dst_over_stat.stat_expect_is_dir( dst_over )) {
-		return FAIL_FAILED();
-	}
-
 
 	// src file must exist, as a file, or as ...
 	if( !src_stat.stat( (STR0) src_name )) {
@@ -78,8 +123,19 @@ bool sender_t::copy_src_name_dst_try(
 		return FAIL_FAILED();
 	}
 
-	// 31 BIT warning
+	// total file size
 	size_full = src_stat.st.st_size;
+
+	// warn if zero size
+	if( size_full == 0 ) {
+		WARN("CODE error forgot to stat or ZERO size file");
+		// check mtime
+		// return FAIL("CODE error forgot ot stat");
+	}
+
+	// 31 BIT warning
+	// we cope with huge files, but mention it
+	// even 2G is half the usual limit
 	if( size_full >> 31 ) {
 		float szg = size_full;
 		szg /= 1024; // K
@@ -88,20 +144,13 @@ bool sender_t::copy_src_name_dst_try(
 		WARN("SIZE more than 2G, %4.2fG", szg);
 	}
 
-	if( size_full == 0 ) {
-		WARN("CODE error forgot to stat or ZERO size file");
-		// check mtime
-		// return FAIL("CODE error forgot ot stat");
-	}
-
 	// get BASENAME and gen some temp names // from src_name
 	str1 dst_name_tmp;	// copy into this
 	str1 dst_name;		// timestamped final copy // name of
+
 	// decode src_name
 	dir_name_ext src_dir_name_ext;
 	if(!src_dir_name_ext.decode_filename(src_name)) return FAIL_FAILED();
-
-//	src_dir_name_ext.test_print();
 
 	// temp name // partial copy restart
 	// we can take the EXT out because it is a mangled filename
@@ -196,33 +245,43 @@ bool sender_t::copy_src_name_dst_try(
 	// write to mid file
 	// LOOK for restart
 	// open RW +- CREATE // seek zero or stat.midfile.size_part
-	u64 size_part = 0;
+	u64 size_part = 0; // previous run left _part already copied
 	if( !dst_stat.stat_quiet( (STR0) dst_name_tmp )) {
+		// stat_quiet returns TRUE when filename does not exist
+		// but it returns FALSE if some other error happens
 		return FAIL_FAILED(); // stat failed // not absent 
 	}
-	if( dst_stat.linked_file_type != is_absent ) {
+	if( dst_stat.linked_file_type == is_absent ) {
+		// absent file means start copy from beginning
+		if(!fd_dst.open_RW_CREATE( (STR0) dst_name_tmp )) {
+			return FAIL_FAILED();
+		}
+	} else {
 		// exists means restart
+		// we really should be only files by now
+		// others were found above
+		// .tmp is our unique filename, and it is dir or something odd
+		// must be a file
 		if( dst_stat.linked_file_type != is_file ) {
 			return FAIL("%s not is_file", (STR0) dst_name_tmp );
 		}
+		// size_part == amount already copied
 		size_part = dst_stat.st.st_size;
 		if(!fd_dst.open_RW( (STR0) dst_name_tmp )) {
 			return FAIL_FAILED();
 		}
 		// TODO TEST on 32 bit CPU
+		// seek both src and dst to size_part
 		if(!fd_dst.seek_SET_64( size_part )) { // must match fd_dst
 			return FAIL_FAILED();
 		}
 		if(!fd_src.seek_SET_64( size_part )) { // must match fd_dst
 			return FAIL_FAILED();
 		}
-	} else {
-		if(!fd_dst.open_RW_CREATE( (STR0) dst_name_tmp )) {
-			return FAIL_FAILED();
-		}
 	}
-
+	// amount left to do is size_left
 	u64 size_left = size_full - size_part;
+
 	// end // size_part
 	int size_block = 1024 * 32; // 32K is DVD block size
 	static const int loops_per_sync = 1000; // 500; // 100; // 50;
@@ -236,7 +295,6 @@ bool sender_t::copy_src_name_dst_try(
 	time_t time_last_fsync; // rounded seconds
 	time_t time_right_now; // rounded seconds
 	time_last_fsync = time_last_sync = time_right_now = time(NULL);
-	u64 size_sent = 0;
 //	u64 size_sent_tide = 1024*1024; // 1 MB ~ 20 seconds 
 	u64 size_sent_tide = 512*1024; // 256 K ~ 5 seconds // ?
 	while (size_left > 0) {
@@ -249,30 +307,18 @@ bool sender_t::copy_src_name_dst_try(
 				time_last_fsync = time_right_now ;
 				// every 5 seconds // if check within that sec
 				if(0) {
-					e_print("f_");
-					if(! fd_dst.fsync() ) {
+					if(!call_fsync() )
 						return FAIL_FAILED();
-					}
-					e_print("\b");
-					size_sent = 0;
 				}
 				if(1) {
-					e_print("d_");
-					if(! fd_dst.fdatasync() ) {
+					if(!call_fdatasync()) 
 						return FAIL_FAILED();
-					}
-					size_sent = 0;
-					e_print("\b");
 				}
 			} else {
 				// every second other than multiple
 				if(1) {
-					e_print("d_");
-					if(! fd_dst.fdatasync() ) {
+					if(!call_fdatasync()) 
 						return FAIL_FAILED();
-					}
-					size_sent = 0;
-					e_print("\b");
 				}
 			}
 			// restart loop counter // every second
@@ -284,12 +330,8 @@ bool sender_t::copy_src_name_dst_try(
 				loops_count = loops_per_sync;
 				// so why is it showing 0 when pausing not 100
 				if(0) {
-					e_print("d_");
-					if(! fd_dst.fdatasync() ) {
-						// syncs not working as expected
+					if(!call_fdatasync()) 
 						return FAIL_FAILED();
-					}
-					e_print("\b");
 				}
 			}
 		}
@@ -305,7 +347,7 @@ bool sender_t::copy_src_name_dst_try(
 	//	if(!loops_count) e_print("\n");
 	//	e_print("\n");
 	//	fflush(0); // e_print does not need fflush(0)
-		int sz_write = size_block;
+		int sz_write = size_block; // also size_block == sizeof( buff )
 		if( sz_write > size_left ) {
 			sz_write = (int) size_left;
 		}
@@ -323,6 +365,7 @@ bool sender_t::copy_src_name_dst_try(
 		if( wrote != sz_write ) {
 			FAIL("ERROR WRITE expected %d got %d", sz_write, wrote);
 			sleep(2);
+			return FAIL_FAILED(); // try complete restart
 			continue;
 		}
 		size_left -= wrote;
@@ -331,34 +374,27 @@ bool sender_t::copy_src_name_dst_try(
 		if(size_sent >= size_sent_tide) {
 
 			if(1) {
-				e_print("D_");
-				if(! fd_dst.fdatasync() ) {
-					// syncs not working as expected
+				if(!call_fdatasync()) 
 					return FAIL_FAILED();
-				}
-				e_print("\b");
 			}
 
 			if(0) {
-				e_print("F_");
-				if(! fd_dst.fsync() ) {
+				if(!call_fsync() )
 					return FAIL_FAILED();
-				}
-				e_print("\b");
 			}
 
-			size_sent = 0;
 		}
 	} // while
 	e_print("\r");
-	if(! fd_dst.fsync() ) { // also write inode
+	if(!call_fsync() )
 		return FAIL_FAILED();
-	}
-	if(! fd_dst.close() ) {
+	if(!call_fdatasync()) 
 		return FAIL_FAILED();
-	}
+	if(! fd_dst.close() ) 
+		return FAIL_FAILED();
 	// if there was an actual fail, this point is not reached
 
+	// clone mtime // but not perms ?? // not UID // 
 	if(!src_stat.utime_to_filename( (STR0) dst_name_tmp ) ) {
 		return FAIL_FAILED();
 	}
@@ -402,4 +438,5 @@ bool copy_src_name_dst(
 		INFO("retry %d of %d", i+1, loops );
 	}
 	return FAIL_FAILED();
- }
+}
+
